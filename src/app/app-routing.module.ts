252df6a94036b9f18ef52ca5ff5211b790b3f05d
disamiron <mirronovdenis@gmail.com>
2023-11-05 18:22:27 +0300

    add navigation | remove kit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { eRevisionValues } from './shared/constants';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { KitPageComponent } from './pages/kit-page/kit-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
-  },
-  {
-    path: eRevisionValues.kit,
-    component: KitPageComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { KitPageComponent } from './pages/kit-page/kit-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, KitPageComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
